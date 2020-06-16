161794120d19e42af1c31c69e6c19ccdde90f817
Austin Chen <akrolsmir@gmail.com>
2020-06-16 16:28:29 -0700

    Fix column ordering and predict some ratings

--- recommend.py
+++ recommend.py
@@ -36,8 +36,8 @@ def writeJson(filename, data):
 # Format data for surprise's collaborative filtering algorithms
 # https://surprise.readthedocs.io/en/stable/getting_started.html#load-dom-dataframe-py
 ratings_dict = {
-    'card': [],  # item: LoR Card ID
     'deck': [],  # user: RuneTiera Deck ID
+    'card': [],  # item: LoR Card ID
     'count': []  # rating: count of cards in the deck
 }
 
@@ -48,19 +48,36 @@ for run in runs:
         counts[card] = counts[card] + 1 if card in counts else 1
 
     for card, count in counts.items():
-        ratings_dict['card'].append(card)
         ratings_dict['deck'].append(run['id'])
+        ratings_dict['card'].append(card)
         ratings_dict['count'].append(count)
 
 df = pd.DataFrame(ratings_dict)
 df
 
-# A reader is still needed but only the rating_scale param is requiered.
-# TODO: Update to be e.g. 1 to 3? It's rare to have 5 of a single card
-reader = Reader(rating_scale=(1, 5))
+# A reader is still needed but only the rating_scale param is required.
+# TODO: Update to be e.g. 0 to 3? It's rare to have 5 of a single card
+reader = Reader(rating_scale=(0, 5))
 
 # The columns must correspond to user id, item id and ratings (in that order).
-data = Dataset.load_from_df(df[['card', 'deck', 'count']], reader)
+data = Dataset.load_from_df(df[['deck', 'card', 'count']], reader)
 
-# We can now use this dataset as we please, e.g. calling cross_validate
-cross_validate(NormalPredictor(), data, cv=2, verbose=True)
+# Now try predicting a rating
+# From https://surprise.readthedocs.io/en/stable/getting_started.html#train-on-a-whole-trainset-and-the-predict-method
+
+# Retrieve the trainset.
+trainset = data.build_full_trainset()
+trainset
+
+# Build an algorithm, and train it.
+algo = KNNBasic()
+algo.fit(trainset)
+
+# Check how likely this deck would want Back to Back (actually had 3)
+# Deck: https://runetiera.com/draft-viewer?run=8Y8ZjejfT
+pred = algo.predict('8Y8ZjejfT', '01DE041', r_ui=3, verbose=True)
+# They Who Endure
+pred = algo.predict('8Y8ZjejfT', '01FR034', r_ui=0, verbose=True)
+# Now for https://runetiera.com/draft-viewer?run=JAbG8Neme
+pred = algo.predict('JAbG8Neme', '01DE041', r_ui=2, verbose=True)
+pred = algo.predict('JAbG8Neme', '01FR034', r_ui=1, verbose=True)
