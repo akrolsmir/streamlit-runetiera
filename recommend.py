897da5781148d987dcfd14c0377918cdc639af86
Austin Chen <akrolsmir@gmail.com>
2020-06-16 15:31:42 -0700

    Write a subset of runs into runs.json

--- recommend.py
+++ recommend.py
@@ -1,7 +1,31 @@
 import json
 import streamlit as st
 
-with open('data/users.json', encoding='utf-8') as json_file:
-    users = json.load(json_file)
-    for user in users[0:10]:
-        st.write(user)
+import pandas as pd
+import numpy as np
+
+
+# @st.cache(suppress_st_warning=True)
+def loadJson(filename):
+    st.write('Cache miss')
+    with open(filename, encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+
+def writeJson(filename, data):
+    with open(filename, 'w') as out_file:
+        json.dump(data, out_file)
+
+
+# Read from all users, and output to just runs.
+# runs = []
+# users = loadJson('data/users.json')
+# for user in users[0:50]:
+#     for run in user['runs']:
+#         st.write('run')
+#         runs.append(run)
+# writeJson('data/runs.json', runs)
+
+runs = loadJson('data/runs.json')
+for run in runs:
+    st.write(run['id'])
